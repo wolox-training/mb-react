2c5876c429fe4a4ec227c29b3ad4677226de48d7
Pablo Ferro <pablo.ferro@wolox.com.ar>
2021-09-20 12:23:12 -0300

    Initial bootstrap commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,8 @@
+import Home from 'screens/Home';
+import 'scss/application.scss';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+
+import 'scss/application.scss';
+
+import 'config/i18n';
+import App from 'components/App';
+
+import reportWebVitals from './reportWebVitals';
+
+const renderApp = () => {
+  render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+// Render once
+renderApp();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
