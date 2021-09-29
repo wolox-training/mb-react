29b9338705451ddca183cbc8241ae771b0b513f7
MicaelaBalza <micaela.balza@wolox.com.ar>
2021-09-29 13:15:01 -0300

    Refactor Button component.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,17 +4,20 @@ import classNames from 'classnames';
 import styles from './styles.module.scss';
 
 interface Props extends HTMLProps<HTMLButtonElement> {
-  type?: 'button' | 'submit';
   className?: string;
   text: string;
+  type?: 'button' | 'submit';
   variant?: 'contained' | 'outlined' | 'text';
 }
 
-function Button({ className = '', disabled = false, text, variant = 'contained', ...props }: Props) {
-  const btnClasses = classNames(styles[variant], { [styles.disabled]: disabled }, className);
-
+function Button({ className = '', disabled = false, text, type = 'button', variant = 'contained' }: Props) {
   return (
-    <button className={btnClasses} disabled={disabled} type="button" {...props}>
+    <button
+      className={classNames(styles[variant], { [styles.disabled]: disabled }, className)}
+      disabled={disabled}
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+    >
       {text}
     </button>
   );
