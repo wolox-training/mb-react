cec26fc69374b5f6de23ff69893ead18c28afe47
MicaelaBalza <micaela.balza@wolox.com.ar>
2021-10-07 15:16:05 -0300

    Add Login screen.

---
+++ src/screens/Login/i18n.ts
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+
+i18next.addResources('es', 'Login', {
+  firstName: 'Nombre',
+  lastName: 'Apellido',
+  email: 'Email',
+  password: 'Password',
+  passwordConfirmation: 'Confirmación de Password',
+  signUp: 'Sign Up',
+  login: 'Login',
+  changeLanguage: 'Change Language',
+  registeredEmailError: 'Ese email ya está registrado'
+});
+
+i18next.addResources('en', 'Login', {
+  firstName: 'First Name',
+  lastName: 'Last Name',
+  email: 'Email',
+  password: 'Password',
+  passwordConfirmation: 'Confirm Password',
+  signUp: 'Sign Up',
+  login: 'Login',
+  changeLanguage: 'Cambiar idioma',
+  registeredEmailError: 'Email already registered'
+});
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,8 @@
+// import logo from './assets/wolox-logo.png';
+// import styles from './styles.module.scss';
+
+function Login() {
+  return <h1>Login</h1>;
+}
+
+export default Login;
