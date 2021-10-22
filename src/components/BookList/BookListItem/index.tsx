96ccbdd2f66b5ff14a48af1fb52c2260a939fdbb
MicaelaBalza <micaela.balza@wolox.com.ar>
2021-10-22 19:28:56 -0300

    Create BookListItem component.

---
+++ src/components/BookList/BookListItem/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss';
+
+export interface Book {
+  title: string;
+  author: string;
+  id?: number;
+  imageUrl: string;
+}
+
+function BookListItem({ title, author, imageUrl }: Book) {
+  return (
+    <div className={`column ${styles.bookCard}`}>
+      <img src={imageUrl} className={`m-bottom-3 ${styles.bookCover}`} />
+      <p className={`m-bottom-2 ${styles.title}`}>{title}</p>
+      <p className={styles.author}>{author}</p>
+    </div>
+  );
+}
+
+export default BookListItem;
